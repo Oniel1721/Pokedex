bb4d4a928875b5eac59d20c342e604f288ff6481
Oniel_Santos <Oniel@gmail.com>
2020-09-08 20:46:51 -0300

    template-react

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebpackPlugin({ 
+    template: "./src/index.html", 
+    filename: "./index.html" }) 
+    
+module.exports = { 
+    entry: "./src", 
+    output: { path: path.resolve(__dirname, "public"), filename: "bundle.js", }, 
+    module: { rules: [ { test: /\.(js|jsx)/, exclude: /node_modules/, use: ["babel-loader"], }, ], },
+    resolve: { extensions: ["*", ".js", ".jsx"], }, plugins: [htmlPlugin] 
+};
